3c4144506574bf3f86bd8c8d5c5be9da78bf95c1
Rafal <rafapr@onet.pl>
2018-11-01 18:54:49 +0100

    Change returning types for easier upcoming changes

--- include/Series.h
+++ include/Series.h
@@ -14,25 +14,24 @@ class Series : public Movie {
 private:
 
     int numberOfEpisodes;
-    int episodeDurationInMinutes;
-    std::vector< DayOfWeek > broadcastDays;
+    std::vector<std::string> broadcastDays;
 public:
     Series(const std::string &name, const std::string &description, int rate, int durationInMinutes,
-           int numberOfEpisodes, int episodeDurationInMinutes, const std::vector<DayOfWeek> &broadcastDays);
+           const std::vector<std::string> &broadcastDays,int numberOfEpisodes);
+
+    Series(const std::string &name, const std::string &description, int rate, int durationInMinutes,
+           int numberOfEpisodes, const std::vector<std::string> &broadcastDays);
 
 public:
     int getNumberOfEpisodes() const;
 
     void setNumberOfEpisodes(int numberOfEpisodes);
 
-    int getEpisodeDurationInMinutes() const;
+    const std::vector<std::string> &getBroadcastDays() const;
 
-    void setEpisodeDurationInMinutes(int episodeDurationInMinutes);
+    void setBroadcastDays(const std::vector<std::string> &broadcastDays);
 
-    const std::vector<DayOfWeek> &getBroadcastDays() const;
-
-    void setBroadcastDays(const std::vector<DayOfWeek> &broadcastDays);
-
+    std::string toString();
 
 };
 
